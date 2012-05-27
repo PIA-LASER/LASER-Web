633dcb923e8efc5ba4595a5fced73cf8f08ac278
Sven Pfleiderer <sg+github@roothausen.de>
2012-05-27 11:49:30 +0200

    Add test to verify returned user names

--- test/test_user.rb
+++ test/test_user.rb
@@ -12,6 +12,10 @@ class TestUserAPI < MiniTest::Unit::TestCase
     Sinatra::Application
   end
 
+  def setup
+    clear_users
+  end
+
   def test_users_amount
     add_users('test', 10)
     get '/api/users'
@@ -19,6 +23,14 @@ class TestUserAPI < MiniTest::Unit::TestCase
     assert last_response.content_type =~ /json/
     users = JSON.parse(last_response.body)
     assert_equal users.size, 10
+  end
+
+  def test_users_names
+    expected_users = add_users('test', 10)
+    get '/api/users'
+    assert last_response.ok?
+    actual_users = JSON.parse(last_response.body)
+    assert_equal expected_users.sort, actual_users.sort
   end
 
 end
