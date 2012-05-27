8f5e0a62949d83dd9d8d9161d5aafeeb491243fb
Sven Pfleiderer <sg+github@roothausen.de>
2012-05-27 11:51:10 +0200

    Fix user names in api

--- models/users.rb
+++ models/users.rb
@@ -2,18 +2,20 @@ require "rubygems"
 require "json"
 
 class Users
-  NAMESPACE = 'users'
+  NAMESPACE = 'users.'
 
   def self.all
-    redis.keys("#{NAMESPACE}.*")
+    find("*")
   end
 
   def self.find(name)
-    redis.keys("#{NAMESPACE}.#{name}")
+    redis.keys("#{NAMESPACE}#{name}").map do |user|
+      user.gsub(NAMESPACE, "")
+    end
   end
 
   def self.get(name, rec_amount)
-    redis.zrevrange("#{NAMESPACE}.#{name}", 0, rec_amount)
+    redis.zrevrange("#{NAMESPACE}#{name}", 0, rec_amount)
   end
 
   private
