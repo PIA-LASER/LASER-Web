93e9a0d7f69a99be812dfcbe2f7ccd737b49c32b
Dominik Huebner <contact@dhuebner.com>
2012-06-19 20:19:22 +0200

    refactored layout and enabled button to display recommendations

--- public/js/laser.js
+++ public/js/laser.js
@@ -1,5 +1,23 @@
 (function () {
-    $('.typeahead').typeahead({
-        source: [ 'foo', 'bar', 'baz', 'bazinga' ]
-      });
-  }());
+  $('.typeahead').typeahead({
+    source: [ 'foo', 'bar', 'baz', 'bazinga' ]
+  });
+}());
+
+getUserRecommendations = function(userId) {
+  $('#links').empty();
+
+  $.getJSON('/api/users/' + userId, function (data) {
+    $.each(data['recommendations'], function (itemId) {
+      $('#links').append('<tr><td>' + data['recommendations'][itemId]['item'] + '</td><td>' + data['recommendations'][itemId]['score'] + '</td></tr>');
+    });
+  }).error(function (data) {
+    if(data.status == 404) {
+      $('#links').append("<div class=\"alert alert-error\">User not found!</div>");
+    }
+  });
+};
+
+$('#recommend').click(function (event) {
+  getUserRecommendations($('#username').val());
+});
