3f8b4e1f886c92ab06dbd0b7497325f69d4e3b56
Sven Pfleiderer <sg+github@roothausen.de>
2012-06-25 14:27:21 +0200

    Remove unused route

--- controllers/users.rb
+++ controllers/users.rb
@@ -19,9 +19,3 @@ get "/api/users/:username/limit/:amount" do
   content_type :json
   Users.get(params[:username], params[:amount].to_i - 1).to_json
 end
-
-get "/api/users/autocomplete" do
-  content_type :json
-  Users.search_keys('*').to_json
-end
-
